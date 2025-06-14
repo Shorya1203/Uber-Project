bf0c988fd421207158fee40805fca15988931d8c
Shorya1203 <shorya1203@gmail.com>
2025-06-15 04:17:36 +0530

    User Model Created

--- Backend/app.js
+++ Backend/app.js
@@ -4,10 +4,21 @@ dotenv.config() ;
 const express = require('express') ;
 const app = express()
 const cors = require('cors') ; 
+
+const userRoutes = require('./routes/user.routes') ; 
+
+
 app.use(cors());
+app.use(express.json()) ; 
+app.use(express.urlencoded({extended : true})) ; 
+
+const connectToDb = require('./database/db') ; 
+connectToDb()
 
 app.get('/', (req, res) => {
     res.send("Hello World") ;
 }) ; 
+
+app.use('/users', userRoutes) ; 
 
 module.exports = app
